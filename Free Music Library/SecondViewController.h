699cddffc271b37337771b56fed29e5106ff284d
Mark Zgaljic <mzgaljic@me.com>
2014-05-20 21:31:29 -0400

    Boiler plate code and project set up.

---
+++ Free Music Library/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Free Music Library
+//
+//  Created by Mark Zgaljic on 5/20/14.
+//  Copyright (c) 2014 Mark Zgaljic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ Free Music Library/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  Free Music Library
+//
+//  Created by Mark Zgaljic on 5/20/14.
+//  Copyright (c) 2014 Mark Zgaljic. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
