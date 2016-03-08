1305de5f08eff800beb3ac3f6a36febde86c884e
Mark Zgaljic <mzgaljic@me.com>
2016-03-07 21:30:52 -0500

    Updated pods

--- Pods/Headers/Public/Google-Mobile-Ads-SDK/GoogleMobileAds/GADRewardBasedVideoAdDelegate.h
+++ Pods/Headers/Public/Google-Mobile-Ads-SDK/GoogleMobileAds/GADRewardBasedVideoAdDelegate.h
@@ -1,1 +1,1 @@
-../../../../Google-Mobile-Ads-SDK/GoogleMobileAdsSdkiOS-7.6.0/GoogleMobileAds.framework/Versions/A/Headers/GADRewardBasedVideoAdDelegate.h
+../../../../Google-Mobile-Ads-SDK/GoogleMobileAdsSdkiOS-7.7.0/GoogleMobileAds.framework/Versions/A/Headers/GADRewardBasedVideoAdDelegate.h
