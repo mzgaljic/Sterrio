286eee11a2085c43fd5da747b1c7a0d5290985d4
Mark Zgaljic <mzgaljic@me.com>
2016-05-30 02:57:39 -0400

    Deleting 'orphaned song records' after a PlaylistItem is deleted and that song was meant to only be visible in the items playlist.

--- PlaylistItem+Utilities.m
+++ PlaylistItem+Utilities.m
@@ -8,6 +8,7 @@
 
 #import "PlaylistItem+Utilities.h"
 #import "NSObject+ObjectUUID.h"
+#import "CoreDataManager.h"
 
 @implementation PlaylistItem (Utilities)
 
@@ -38,8 +39,26 @@
 }
 
 //custom logic to keep existing PlaylistItem indexes valid if songs are deleted from the library
+//custom logic to delete the underlying song if the user wanted it to only be visible in 1 playlist.
 - (void)prepareForDeletion
 {
+    __block NSManagedObjectContext *context = [self managedObjectContext];
+    if(context == nil) {
+        context = [CoreDataManager context];
+    }
+    
+    //NOTE: Normally when a PlaylistItem is deleted, the 'nullify' delete rule takes place.
+    //The song will remain in the general library and only be gone from the playlist. If the user
+    //wanted this song to only be visible from a particular playlist, then we need to perform additional
+    //work here to avoid an 'orphaned record' in core data. Particular - having Song entities floating
+    //around without the user ever knowing.
+    __block BOOL deleteSongWithDelay = NO;
+    if(self.song.smartSortSongName == nil) {
+        //user only wanted it visible in this playlist.
+        deleteSongWithDelay = YES;
+    }
+    
+    //NOTE: Here is the 'most common/normal' PlaylistItem delete logic...
     //the index of every PlaylistItem after this one will be screwed up once this is deleted. fix that...
     Playlist *playlist = self.playlist;
     
@@ -65,6 +84,18 @@
     }];
     
     playlist.playlistItems = [NSSet setWithArray:allItems];
+    //end PlaylistItem incrementing logic.
+    
+    if(deleteSongWithDelay) {
+        int delaySeconds = 1;
+        __weak __block Song *weakSong = self.song;
+        dispatch_time_t delayTime = dispatch_time(DISPATCH_TIME_NOW, delaySeconds * NSEC_PER_SEC);
+        dispatch_after(delayTime, dispatch_get_main_queue(), ^(void) {
+            NSLog(@"Deleting song w/ name: '%@' from entire library.", weakSong.songName);
+            [context deleteObject:weakSong];
+            context = nil;
+        });
+    }
 }
 
 @end
