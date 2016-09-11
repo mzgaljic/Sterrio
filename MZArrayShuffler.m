da0f738cbcf97f9231632b4c06264b9402569550
Mark Zgaljic <mzgaljic@me.com>
2016-09-11 18:25:37 -0400

    Bug fix: shuffling when the first song is already selected would crash the app due to a bad asset statement.

--- MZArrayShuffler.m
+++ MZArrayShuffler.m
@@ -40,7 +40,7 @@
         return;
     }
     NSAssert(count - 1 >= index, @"Index out of range! Index: %lu is larger than the last index of: %lu.", (unsigned long)index, count-1);
-    NSAssert(index > 0, @"Index out of range! Index value of: %lu is less than 0!", (unsigned long)index);
+    NSAssert(index >= 0, @"Index out of range! Index value of: %lu is less than 0!", (unsigned long)index);
     
     //to make the the desired element stays in front. Then shuffle the remainder of the array.
     [(*arrayToModify) exchangeObjectAtIndex:0 withObjectAtIndex:index];
